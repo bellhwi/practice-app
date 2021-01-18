8a3031c037a90f4f781f1cd98c47f0e27f263d9e
Jonghwi <johnnykim94@hotmail.com>
2021-01-19 01:19:23 +0900

    2021-01-19

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,24 +1,56 @@
 const display = document.getElementById('display');
-const calc = document.getElementById('calc');
 let result = '';
 
-function add(char, checkIfCalc) {
-  if (isNaN(char) == true) {
-    result.innerText = result.innerText + char;
-  }
-  if (isNaN(char) == true && checkIfCalc == 'calc') {
-    calc.classList.add('clicked');
+function add(char, id) {
+  if (isNaN(char) == true && char != '.') {
+    // Set the clicked effect on calculate buttons
+    if (document.querySelector('.clicked') == null) {
+      document.getElementById(id).classList.add('clicked');
+    } else {
+      document.querySelector('.clicked').classList.remove('clicked');
+      document.getElementById(id).classList.add('clicked');
+    }
   } else {
-    display.innerText = display.innerText + char;
-    result.innerText = result.innerText + char;
+    // Remove clicked effect when press numbers
+    if (document.querySelector('.clicked') != null) {
+      document.querySelector('.clicked').classList.remove('clicked');
+    }
+    // Prevent multiple zero when the display value is empty
+    if (display.innerText == '' && char == '0') {
+      display.innerText = '';
+    }
+    // Show dot with zero when the display value is empty
+    else if (display.innerText == '' && char == '.') {
+      display.innerText = '0.';
+    }
+    // Display the value
+    else {
+      display.innerText = display.innerText + char;
+      result = result + char;
+    }
   }
 }
 
 function ac() {
-  display.innerText = null;
-  result.innerText = null;
+  display.innerText = '';
+  result = '';
+}
+
+function sign() {
+  if (display.innerText !== '') {
+    if (display.innerText.includes('-') == true) {
+      display.innerText = `${Math.abs(parseInt(display.innerText, 10))}`;
+    } else {
+      display.innerText = `-${display.innerText}`;
+    }
+  }
+}
+
+function percent() {
+  display.innerText = `${parseInt(display.innerText, 10)}` / 100;
 }
 
 function equal() {
-  display.innerText = result.innerText;
+  display.innerText = result;
+  console.log(result);
 }
