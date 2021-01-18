4bf3fe477ab9669125de90610165164c11026ad0
Jonghwi <johnnykim94@hotmail.com>
2021-01-18 17:07:58 +0900

    2021-01-18

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,24 @@
-const number = document.getElementsByName('number');
+const display = document.getElementById('display');
+const calc = document.getElementById('calc');
+let result = '';
 
-function numClickEFX() {
-  number.style.transition = 'all 2s ease';
+function add(char, checkIfCalc) {
+  if (isNaN(char) == true) {
+    result.innerText = result.innerText + char;
+  }
+  if (isNaN(char) == true && checkIfCalc == 'calc') {
+    calc.classList.add('clicked');
+  } else {
+    display.innerText = display.innerText + char;
+    result.innerText = result.innerText + char;
+  }
+}
+
+function ac() {
+  display.innerText = null;
+  result.innerText = null;
+}
+
+function equal() {
+  display.innerText = result.innerText;
 }
