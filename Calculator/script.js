9b97e3394016fad8daf21e32936a8470563b2c63
Jonghwi <johnnykim94@hotmail.com>
2021-01-20 01:52:05 +0900

    2021-01-20

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,7 +1,9 @@
 const display = document.getElementById('display');
-let result = '';
+
+let result = [];
 
 function add(char, id) {
+  // Not a number
   if (isNaN(char) == true && char != '.') {
     // Set the clicked effect on calculate buttons
     if (document.querySelector('.clicked') == null) {
@@ -9,19 +11,46 @@ function add(char, id) {
     } else {
       document.querySelector('.clicked').classList.remove('clicked');
       document.getElementById(id).classList.add('clicked');
+      result = '';
     }
-  } else {
+
+    // Press calculate when display is empty
+    if (char == '*' || '+' || '-' || '/') {
+      if (display.innerText == '') {
+        result = '0' + char;
+      } else {
+        result = result + char;
+      }
+    }
+  }
+  // Number
+  else {
     // Remove clicked effect when press numbers
     if (document.querySelector('.clicked') != null) {
+      display.innerText = '';
       document.querySelector('.clicked').classList.remove('clicked');
     }
+
     // Prevent multiple zero when the display value is empty
     if (display.innerText == '' && char == '0') {
       display.innerText = '';
     }
     // Show dot with zero when the display value is empty
     else if (display.innerText == '' && char == '.') {
-      display.innerText = '0.';
+      if (display.innerText.includes('.') == true) {
+        return;
+      } else {
+        display.innerText = '0.';
+        result = '0.';
+      }
+    }
+    // Prevent multiple dot when there is a dot already.
+    else if (display.innerText != '' && char == '.') {
+      if (display.innerText.includes('.') == true) {
+        return;
+      } else {
+        display.innerText = display.innerText + '.';
+      }
     }
     // Display the value
     else {
@@ -34,23 +63,37 @@ function add(char, id) {
 function ac() {
   display.innerText = '';
   result = '';
+  if (document.querySelector('.clicked') != null) {
+    document.querySelector('.clicked').classList.remove('clicked');
+  }
 }
 
 function sign() {
-  if (display.innerText !== '') {
-    if (display.innerText.includes('-') == true) {
-      display.innerText = `${Math.abs(parseInt(display.innerText, 10))}`;
-    } else {
-      display.innerText = `-${display.innerText}`;
-    }
+  if (display.innerText != '') {
+    display.innerText = result = -1 * parseInt(display.innerText, 10);
   }
 }
 
 function percent() {
-  display.innerText = `${parseInt(display.innerText, 10)}` / 100;
+  if (display.innerText != '') {
+    display.innerText = `${parseInt(display.innerText, 10)}` / 100;
+  }
 }
 
 function equal() {
-  display.innerText = result;
-  console.log(result);
+  if (display.innerText !== '') {
+    // Calculate the result string and return to number
+    let calcResult = new Function('return ' + result)();
+
+    // Check if the number is integer or not.
+    if (Number.isInteger(calcResult) == true) {
+      display.innerText = result = calcResult;
+    } else {
+      display.innerText = result = calcResult.toFixed(2);
+    }
+  }
+  // Prevent show 'undefined' when press equal while it's empty, zero or dot.
+  else {
+    display.innerText = '';
+  }
 }
