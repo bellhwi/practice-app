0cd1df41590da4e7485489937fe4714a9d86bdb9
Jonghwi <johnnykim94@hotmail.com>
2021-01-23 01:54:02 +0900

    2021-01-23

--- Meal Finder/script.js
+++ Meal Finder/script.js
@@ -15,13 +15,24 @@ async function getMeal() {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm.value}`
   );
   const data = await res.json();
-  const meals = data.meals[0];
+  const meals = data.meals;
 
-  const thumbnail = meals.strMealThumb;
+  Object.keys(meals).forEach((key) => {
+    const gridItem = document.querySelector('.grid-item');
+    const thumb = meals[key].strMealThumb;
+    Object.keys(thumb).forEach((image) => {
+      gridItem.classList.add('image');
+      gridItem.innerHTML = `<img src="${image}"></img>`;
+    });
 
-  document.querySelector(
-    '.grid-item'
-  ).innerHTML = `<img src="${thumbnail}"></img>`;
+    /*
+    document.querySelector(
+      '.grid-item'
+    ).innerHTML = `<img src="${meals[key].strMealThumb}"></img>`;
+    */
+  });
+
+  document.getElementById(
+    'result'
+  ).innerHTML = `Search results for '${searchTerm.value}':`;
 }
-
-function setMeal() {}
