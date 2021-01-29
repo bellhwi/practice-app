aa19e286fc47c681e8b25481cdb57c5ffa220859
Jonghwi <johnnykim94@hotmail.com>
2021-01-29 20:58:42 +0900

    2021-01-29

--- Meal Finder/script.js
+++ Meal Finder/script.js
@@ -1,4 +1,5 @@
 const searchTerm = document.getElementById('search-bar');
+const mealsEl = document.getElementById('meals');
 
 function search() {
   // Show alert if search bar is empty.
@@ -9,30 +10,27 @@ function search() {
   }
 }
 
-// Fetch meal info
 async function getMeal() {
+  // Display search result
+  document.getElementById(
+    'result'
+  ).innerHTML = `Search results for '${searchTerm.value}':`;
+
+  // Fetch meal info
   const res = await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm.value}`
   );
   const data = await res.json();
   const meals = data.meals;
 
-  Object.keys(meals).forEach((key) => {
-    const gridItem = document.querySelector('.grid-item');
-    const thumb = meals[key].strMealThumb;
-    Object.keys(thumb).forEach((image) => {
-      gridItem.classList.add('image');
-      gridItem.innerHTML = `<img src="${image}"></img>`;
-    });
+  console.log(meals);
 
-    /*
-    document.querySelector(
-      '.grid-item'
-    ).innerHTML = `<img src="${meals[key].strMealThumb}"></img>`;
-    */
-  });
-
-  document.getElementById(
-    'result'
-  ).innerHTML = `Search results for '${searchTerm.value}':`;
+  mealsEl.innerHTML = meals
+    .map(
+      (meal) => `
+  <div>
+  <img src="${meal.strMealThumb}"></img>
+  </div>`
+    )
+    .join('');
 }
